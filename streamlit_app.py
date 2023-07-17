412c77ae4316cb82a2836415cdf9ac088c8b8637
edwardraya16 <edwardraya16@gmail.com>
2023-07-18 06:06:38 +0700

    keras

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@
 
 import streamlit as st
 import os
-from tensorflow.keras.models import load_model
+from tensorflow import keras
+from keras.models import load_model
 from PIL import Image
 import numpy as np
 from skimage import transform
